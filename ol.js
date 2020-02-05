a189e698dd931ca97e38ae5c5b5ff1f65f14e435
kingdzrg <58010386+kingdzrg@users.noreply.github.com>
2020-02-05 21:31:03 +0100

    Update ol.js

--- ol.js
+++ ol.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var adminprefix = "!"
-const developers = ["537675177677291530"]//Toxic Codes
+const developers = ["537675177677291530"]//KinG Codes
 
 client.on('ready', () => {
    console.log(`----------------`);
-   console.log(`Credit Farmm - Script By : KING_Dz `); 
    console.log(`----------------`);
    console.log(`Loadinng`);
    console.log(`Loadinng.`);
@@ -15,41 +14,41 @@ client.on('ready', () => {
    console.log(`----------------`);
 });
 
-client.on('message', message => {//Toxic Codes
-    var argresult = message.content.split(` `).slice(1).join(' ');//Toxic Codes
+client.on('message', message => {//KinG Codes
+    var argresult = message.content.split(` `).slice(1).join(' ');//KinG Codes
       if (!developers.includes(message.author.id)) return;
      
   if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else
-     if (message.content === (adminprefix + "leave")) {//Toxic Codes
+     if (message.content === (adminprefix + "leave")) {//KinG Codes
     message.guild.leave();   //Toxic Codes
   } else  
   if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  client.user.setActivity(argresult, {type:'WATCHING'});//KinG Codes
+      message.channel.send(`**✅   ${argresult}**`)//KinG Codes
   } else
   if (message.content.startsWith(adminprefix + 'setl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+      message.channel.send(`**✅   ${argresult}**`)//KinG Codes
   } else
   if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/zero");
-      message.channel.send(`**✅**`)//Toxic Codes
+      message.channel.send(`**✅**`)//KinG Codes
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-  if (message.content.startsWith(adminprefix + 'setadminprefix')) {//Toxic Codes
+  if (message.content.startsWith(adminprefix + 'setadminprefix')) {//KinG Codes
   client.user.setadminprefix(argresult).then
-      message.channel.send(`Changing Prefix ..**${argresult}** `)//Toxic Codes
+      message.channel.send(`Changing Prefix ..**${argresult}** `)//KinG Codes
 } else
-if (message.content.startsWith(adminprefix + 'setavatar')) {//Toxic Codes
+if (message.content.startsWith(adminprefix + 'setavatar')) {//KinG Codes
   client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);//Toxic Codes
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);//KinG Codes
 }
-});//Toxic Codes
+});//KinG Codes
 
 client.login(process.env.BOT_TOKEN);
